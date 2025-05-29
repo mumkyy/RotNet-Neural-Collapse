2e0de0a7c28795d4ceb7edd3e872be7076ec05fb
Assaf Izhar <assaf.izhar@gmail.com>
2025-05-29 01:05:43 -0400

    fix FeatureClassificationModel.py

--- algorithms/FeatureClassificationModel.py
+++ algorithms/FeatureClassificationModel.py
@@ -1,102 +1,112 @@
-import torch
-import utils
 import time
-
+import torch
 from . import Algorithm
 from pdb import set_trace as breakpoint
 
 
+# ------------------------------------------------------------------ #
+# helpers                                                            #
+# ------------------------------------------------------------------ #
 def accuracy(output, target, topk=(1,)):
-    """Computes the precision@k for the specified values of k"""
+    """Return list with top-k accuracies as plain floats."""
     maxk = max(topk)
     batch_size = target.size(0)
 
-    _, pred = output.topk(maxk, 1, True, True)
-    pred = pred.t()
-    correct = pred.eq(target.view(1, -1).expand_as(pred))
+    _, pred = output.topk(maxk, dim=1, largest=True, sorted=True)  # B × maxk
+    pred = pred.t()                                                # maxk × B
+    correct = pred.eq(target.view(1, -1).expand_as(pred))          # maxk × B
 
-    return [correct[k - 1].float().mul_(100.0 / batch_size).item() for k in topk]
+    res = []
+    for k in topk:
+        corr_k = correct[:k].reshape(-1).float().sum(0)
+        res.append((corr_k * 100.0 / batch_size).item())           # ★
+    return res
 
+
+# ------------------------------------------------------------------ #
+# model                                                              #
+# ------------------------------------------------------------------ #
 class FeatureClassificationModel(Algorithm):
     def __init__(self, opt):
         self.out_feat_keys = opt['out_feat_keys']
-        Algorithm.__init__(self, opt)
+        super().__init__(opt)
 
+    # --------------------------------------------------------------
+    # infrastructure
+    # --------------------------------------------------------------
     def allocate_tensors(self):
-        self.tensors = {}
-        self.tensors['dataX'] = torch.FloatTensor()
-        self.tensors['labels'] = torch.LongTensor()
+        self.tensors = {
+            'dataX':  torch.FloatTensor(),
+            'labels': torch.LongTensor()
+        }
 
+    # --------------------------------------------------------------
+    # training / eval hooks
+    # --------------------------------------------------------------
     def train_step(self, batch):
         return self.process_batch(batch, do_train=True)
 
     def evaluation_step(self, batch):
         return self.process_batch(batch, do_train=False)
 
+    # --------------------------------------------------------------
+    # core logic
+    # --------------------------------------------------------------
     def process_batch(self, batch, do_train=True):
-        #*************** LOAD BATCH (AND MOVE IT TO GPU) ********
-        start = time.time()
+        # ---------- load batch ------------------------------------
+        t0 = time.time()
         self.tensors['dataX'].resize_(batch[0].size()).copy_(batch[0])
         self.tensors['labels'].resize_(batch[1].size()).copy_(batch[1])
-        dataX = self.tensors['dataX']
-        labels = self.tensors['labels']
-        batch_load_time = time.time() - start
-        #********************************************************
+        dataX   = self.tensors['dataX']
+        labels  = self.tensors['labels']
+        load_tm = time.time() - t0
 
-        #********************************************************
-        start = time.time()
-        out_feat_keys = self.out_feat_keys
-        finetune_feat_extractor = self.optimizers['feat_extractor'] is not None
-        if do_train: # zero the gradients
-            self.optimizers['classifier'].zero_grad() 
-            if finetune_feat_extractor:
+        # ---------- zero grad & mode ------------------------------
+        finetune = self.optimizers['feat_extractor'] is not None
+        if do_train:
+            self.optimizers['classifier'].zero_grad()
+            if finetune:
                 self.optimizers['feat_extractor'].zero_grad()
             else:
                 self.networks['feat_extractor'].eval()
-        #********************************************************
 
-        #***************** SET TORCH VARIABLES ******************
-        dataX_var =dataX
-        labels_var = labels
-        #********************************************************
-
-        #************ FORWARD PROPAGATION ***********************
-        feat_var = self.networks['feat_extractor'](dataX_var, out_feat_keys=out_feat_keys)
-        if not finetune_feat_extractor:
-            with torch.no_grad():             # no_grad scope
-                if isinstance(feat_var, (list, tuple)):
-                    feat_var = [f.detach() for f in feat_var]
+        # ---------- forward ---------------------------------------
+        feat = self.networks['feat_extractor'](
+            dataX, out_feat_keys=self.out_feat_keys
+        )
+        if not finetune:                       # detach backbone
+            with torch.no_grad():
+                if isinstance(feat, (list, tuple)):
+                    feat = [f.detach() for f in feat]
                 else:
-                    feat_var = feat_var.detach()
-        pred_var = self.networks['classifier'](feat_var)
-        #********************************************************
+                    feat = feat.detach()
 
-        #*************** COMPUTE LOSSES *************************
+        pred = self.networks['classifier'](feat)
+
+        # ---------- loss & metrics --------------------------------
         record = {}
-        if isinstance(pred_var, (list, tuple)):
+        if isinstance(pred, (list, tuple)):
             loss_total = None
-            for i in range(len(pred_var)):
-                loss_this = self.criterions['loss'](pred_var[i], labels_var)
-                loss_total = loss_this if (loss_total is None) else (loss_total + loss_this)
-                record['prec1_c'+str(1+i)] = accuracy(pred_var[i].data, labels, topk=(1,))[0][0]
-                record['prec5_c'+str(1+i)] = accuracy(pred_var[i].data, labels, topk=(5,))[0][0]
+            for i, p in enumerate(pred):
+                loss = self.criterions['loss'](p, labels)
+                loss_total = loss if loss_total is None else loss_total + loss
+                record[f'prec1_c{i+1}'] = accuracy(p, labels, (1,))[0]   # ★
+                record[f'prec5_c{i+1}'] = accuracy(p, labels, (5,))[0]   # ★
         else:
-            loss_total = self.criterions['loss'](pred_var, labels_var)
-            record['prec1'] = accuracy(pred_var, labels, topk=(1,))[0]
-            record['prec5'] = accuracy(pred_var.data, labels, topk=(5,))[0][0]
+            loss_total = self.criterions['loss'](pred, labels)
+            record['prec1'] = accuracy(pred, labels, (1,))[0]            # ★
+            record['prec5'] = accuracy(pred, labels, (5,))[0]            # ★
         record['loss'] = loss_total.item()
-        #********************************************************
 
-        #****** BACKPROPAGATE AND APPLY OPTIMIZATION STEP *******
+        # ---------- backward --------------------------------------
         if do_train:
             loss_total.backward()
             self.optimizers['classifier'].step()
-            if finetune_feat_extractor:
+            if finetune:
                 self.optimizers['feat_extractor'].step()
-        #********************************************************
-        batch_process_time = time.time() - start
-        total_time = batch_process_time + batch_load_time
-        record['load_time'] = 100*(batch_load_time/total_time)
-        record['process_time'] = 100*(batch_process_time/total_time)
 
+        proc_tm = time.time() - t0
+        total_tm = load_tm + proc_tm
+        record['load_time'] = 100 * load_tm / total_tm
+        record['process_time'] = 100 * proc_tm / total_tm
         return record
