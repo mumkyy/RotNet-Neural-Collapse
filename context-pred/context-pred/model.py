9e42eca564451daf9ee89eeddfdfb2bddcb09905
Akshay Rangamani <rangamani.akshay@gmail.com>
2025-11-20 18:00:00 -0500

    Modified AlexNetwork in context_pred to handle 16x16 patches

--- context-pred/context-pred/model.py
+++ context-pred/context-pred/model.py
@@ -2,57 +2,58 @@ import torch
 import torch.nn as nn
 
 class AlexNetwork(nn.Module):
-  def __init__(self,aux_logits = False):
-      super(AlexNetwork, self).__init__()
-      self.cnn = nn.Sequential(
-        nn.Conv2d(3, 96, kernel_size=11, stride=4),
-        nn.ReLU(inplace=True),
-        nn.MaxPool2d(kernel_size=3, stride=2),
-        nn.LocalResponseNorm(96),
+    def __init__(self, aux_logits=False):
+        super(AlexNetwork, self).__init__()
+        self.cnn = nn.Sequential(
+            nn.Conv2d(3, 96, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.GroupNorm(32,96),
+            
+            nn.Conv2d(96, 384, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),  # 16x16 -> 8x8
+            nn.GroupNorm(96,384),
+            
+            nn.Conv2d(384, 384, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.GroupNorm(96, 384),
+            
+            nn.Conv2d(384, 384, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.GroupNorm(96, 384),
+            
+            nn.Conv2d(384, 256, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.GroupNorm(64, 256),
+            nn.MaxPool2d(kernel_size=2, stride=2),  # 8x8 -> 4x4
+        )
         
-        nn.Conv2d(96, 384, kernel_size=5, stride = 2,padding = 2),
-        nn.ReLU(inplace=True),
-        nn.MaxPool2d(kernel_size=3, stride=2),
-        nn.LocalResponseNorm(384),
+        # FC layers - adjusted input size for 4x4x256 = 4096
+        self.fc6 = nn.Sequential(
+            nn.Linear(256 * 4 * 4, 4096),
+            nn.ReLU(inplace=True),
+            nn.LayerNorm(4096),
+        )
         
-        nn.Conv2d(384, 384, kernel_size=3, stride=1,padding = 1),
-        nn.ReLU(inplace=True),
-        nn.BatchNorm2d(384),
-        
-        nn.Conv2d(384, 384, kernel_size=3, stride=1,padding = 1),
-        nn.ReLU(inplace=True),
-        nn.BatchNorm2d(384),
-        
-        nn.Conv2d(384, 256, kernel_size=3, stride=1,padding = 1),
-        nn.ReLU(inplace=True),
-        nn.BatchNorm2d(256),
-        nn.MaxPool2d(kernel_size=3, stride=2,padding = 1),
-      )
-      self.fc6 = nn.Sequential(
-        nn.Linear(256,4096),
-        nn.ReLU(inplace=True),
-        nn.BatchNorm1d(4096),
-      )
-      self.fc = nn.Sequential(
-        nn.Linear(2*4096,4096),
-        nn.ReLU(inplace=True),
+        self.fc = nn.Sequential(
+            nn.Linear(2 * 4096, 4096),
+            nn.ReLU(inplace=True),
 
-        nn.Linear(4096, 4096),
-        nn.ReLU(inplace=True),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
 
-        nn.Linear(4096, 8)
-      )
+            nn.Linear(4096, 8)
+        )
 
-  def forward_once(self, x):
-    output= self.cnn(x)
-    output = output.view(output.size()[0], -1)
-    output = self.fc6(output)
-    return output
+    def forward_once(self, x):
+        output = self.cnn(x)
+        output = output.view(output.size()[0], -1)
+        output = self.fc6(output)
+        return output
 
-
-  def forward(self, uniform_patch, random_patch):
-    output_fc6_uniform = self.forward_once(uniform_patch)
-    output_fc6_random = self.forward_once(random_patch)
-    output = torch.cat((output_fc6_uniform,output_fc6_random), 1)
-    output = self.fc(output)
-    return output, output_fc6_uniform, output_fc6_random
+    def forward(self, uniform_patch, random_patch):
+        output_fc6_uniform = self.forward_once(uniform_patch)
+        output_fc6_random = self.forward_once(random_patch)
+        output = torch.cat((output_fc6_uniform, output_fc6_random), 1)
+        output = self.fc(output)
+        return output, output_fc6_uniform, output_fc6_random
