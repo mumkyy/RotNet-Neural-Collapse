db9e2a412c145eac40177f142f74607f48ee7da2
Assaf Izhar <ai342@njit.edu>
2025-11-21 00:31:02 -0500

    made patching dataset deterministic and with 8 patch pairs per image

--- context-pred/context-pred/data.py
+++ context-pred/context-pred/data.py
@@ -15,15 +15,16 @@ class MyDataset(Dataset):
     self.patch_dim, self.gap = patch_dim, gap
     self.transform = transform
     self.base_ds = base_ds
+    self.patch_loc_arr = [(1, 1), (1, 2), (1, 3),(2, 1),(2, 3),(3, 1), (3, 2), (3, 3)]
+    self.num_locs = len(self.patch_loc_arr)
   
-  def get_patch_from_grid(self, image, patch_dim, gap):
+  def get_patch_from_grid(self, image, patch_dim, gap, loc_index):
     image = np.array(image)
 
     offset_x, offset_y = image.shape[0] - (patch_dim*3 + gap*2), image.shape[1] - (patch_dim*3 + gap*2)
-    start_grid_x, start_grid_y = np.random.randint(0, offset_x), np.random.randint(0, offset_y)
-    patch_loc_arr = [(1, 1), (1, 2), (1, 3), (2, 1), (2, 3), (3, 1), (3, 2), (3, 3)]
-    loc = np.random.randint(len(patch_loc_arr))
-    tempx, tempy = patch_loc_arr[loc]
+    start_grid_x, start_grid_y = offset_x//2, offset_y//2
+    loc = loc_index
+    tempx, tempy = self.patch_loc_arr[loc]
     
     patch_x_pt = start_grid_x + patch_dim * (tempx-1) + gap * (tempx-1)
     patch_y_pt = start_grid_y + patch_dim * (tempy-1) + gap * (tempy-1)
@@ -38,11 +39,14 @@ class MyDataset(Dataset):
     return uniform_patch, random_patch, random_patch_label
 
   def __len__(self):
-    return len(self.base_ds)
+    return len(self.base_ds) * 8
   
   def __getitem__(self, index):
-    img, _ = self.base_ds[index]
-    uniform_patch, random_patch, random_patch_label = self.get_patch_from_grid(img, self.patch_dim, self.gap)
+    img_index = index // 8
+    loc_index = index % self.num_locs
+
+    img, _ = self.base_ds[img_index]
+    uniform_patch, random_patch, random_patch_label = self.get_patch_from_grid(img, self.patch_dim, self.gap, loc_index)
 
     # Dropped color channels 2 and 3 and replaced with gaussian noise(std ~1/100 of the std of the remaining channel)
     uniform_patch[:, :, 1] = np.random.normal(0.485, 0.01 * np.std(uniform_patch[:, :, 0]), (uniform_patch.shape[0],uniform_patch.shape[1]))
