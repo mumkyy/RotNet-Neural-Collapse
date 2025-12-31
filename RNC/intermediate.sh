677692173240791498115e3664ca288ea9428dfa
Assaf Izhar <assaf.izhar@gmail.com>
2025-12-31 18:11:45 -0500

    fixed inter.sh

--- RNC/intermediate.sh
+++ RNC/intermediate.sh
@@ -2,8 +2,8 @@
 set -euo pipefail
 
 # Work from project root
-COLL="config/CIFAR10/RotNet/MSE/Collapsed/convclassifier/conv1/CIFAR10_ConvClassifier_on_RotNet_NIN4blocks_Conv2_feats_Collapsed_MSE.py"
-NOT="config/CIFAR10/RotNet/MSE/Not_Collapsed/convclassifier/conv1/CIFAR10_ConvClassifier_on_RotNet_NIN4blocks_Conv2_feats_Not_Collapsed_MSE.py"
+COLL="config/CIFAR10/RotNet/MSE/Collapsed/convclassifier/conv2/CIFAR10_ConvClassifier_on_RotNet_NIN4blocks_Conv2_feats_Collapsed_MSE.py"
+NOT="config/CIFAR10/RotNet/MSE/Not_Collapsed/convclassifier/conv2/CIFAR10_ConvClassifier_on_RotNet_NIN4blocks_Conv2_feats_Not_Collapsed_MSE.py"
 
 convs=(1 2 3 4)
 
@@ -33,7 +33,7 @@ for conv in "${convs[@]}"; do
     hooks+=("conv${conv}.Block${conv}_${sp}")
   fi
 
-  ((count++))
+  ((count+=1))
 done
 
 hooks+=("classifier")
@@ -64,15 +64,5 @@ for i in "${!hooks[@]}"; do
   sed -E -i "s|^config\\['out_feat_keys'\\][[:space:]]*=.*|config['out_feat_keys'] = ['${hook}']|" "$NOT_EXP"
 
   echo "Wrote $NOT_EXP"
-
-  job_safe="${hook//./_}"
-
-  # collapsed
-  jid_coll=$(sbatch --parsable --job-name="gpu_collapsed_${job_safe}" --export=ALL,EXP_NAME="$COLL_EXP" ../scripts/submit.sh)
-  echo "Submitted collapsed: $jid_coll"
-
-  # not collapsed
-  jid_not=$(sbatch --parsable --job-name="gpu_not_collapsed_${job_safe}" --export=ALL,EXP_NAME="$NOT_EXP" ../scripts/submit.sh)
-  echo "Submitted not collapsed: $jid_not"
 
 done
