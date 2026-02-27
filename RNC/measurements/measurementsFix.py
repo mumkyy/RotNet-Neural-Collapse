a85fced07b575b5aab60dd4b240bf12ba2763be1
assaf.izhar@gmail.com <assaf.izhar@gmail.com>
2026-02-27 09:48:29 -0500

    testing a change in determinsm for measurements

--- RNC/measurements/measurementsFix.py
+++ RNC/measurements/measurementsFix.py
@@ -22,8 +22,10 @@ def set_seed(seed: int) -> None:
     np.random.seed(seed)
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
-    torch.backends.cudnn.deterministic = True
-    torch.backends.cudnn.benchmark = False
+    #torch.backends.cudnn.deterministic = True
+    #torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = False
+    torch.backends.cudnn.benchmark = True
 
 
 def parse_float_list(s: Optional[str]) -> Optional[List[float]]:
