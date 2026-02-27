e58793684afa4f0bac50761e4a203c5f5578e7b1
assaf.izhar@gmail.com <assaf.izhar@gmail.com>
2026-02-27 01:50:37 -0500

    lower lr

--- RNC/config/Imagenette/jigsaw9/AlexNet/MSE/Not_Collapsed/backbone/Imagenette_Jigsaw9_AlexNet_Not_Collapsed_MSE_Four_classes_jitter_colordist_test_noCDjitter.py
+++ RNC/config/Imagenette/jigsaw9/AlexNet/MSE/Not_Collapsed/backbone/Imagenette_Jigsaw9_AlexNet_Not_Collapsed_MSE_Four_classes_jitter_colordist_test_noCDjitter.py
@@ -33,7 +33,7 @@ net_opt = {}
 net_opt['num_classes'] = 4
 
 networks = {}
-net_optim_params = {'optim_type': 'sgd', 'lr': 0.1, 'momentum':0.9, 'weight_decay': 0, 'nesterov': True, 'LUT_lr':[(60, 0.1),(120, 0.02),(160, 0.004),(200, 0.0008)]}
+net_optim_params = {'optim_type': 'sgd', 'lr': 0.001, 'momentum':0.9, 'weight_decay': 0, 'nesterov': True, 'LUT_lr':[(60, 0.1),(120, 0.02),(160, 0.004),(200, 0.0008)]}
 networks['model'] = {'def_file': 'architectures/AlexNet.py', 'pretrained': None, 'opt': net_opt, 'optim_params': net_optim_params}
 config['networks'] = networks
 
