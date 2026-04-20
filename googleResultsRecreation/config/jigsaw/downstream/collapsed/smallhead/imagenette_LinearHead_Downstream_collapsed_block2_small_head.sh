f2e06aca2b698e88178e6adb0df5cc0f98bbc722
assaf.izhar@gmail.com <assaf.izhar@gmail.com>
2026-04-20 13:26:27 -0400

    all blocks small head

---
+++ googleResultsRecreation/config/jigsaw/downstream/collapsed/smallhead/imagenette_LinearHead_Downstream_collapsed_block2_small_head.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+python train_and_eval.py \
+  --workdir /project/amr239/gma35/RotNet-Neural-Collapse/googleResultsRecreation/workdirs/DOWNSTREAM_COLLAPSED/LinearHead_Jig9_10Perms_40Epoch/block_2_small \
+  --task downstream \
+  --architecture resnet50 \
+  --dataset imagenette \
+  --dataset_dir /project/amr239/gma35/RotNet-Neural-Collapse/RNC/datasets/Imagenette/imagenette2-160 \
+  --permutations_path /project/amr239/gma35/RotNet-Neural-Collapse/googleResultsRecreation/permutations_100_max.bin \
+  --train_split train \
+  --val_split val \
+  --batch_size 32 \
+  --eval_batch_size 4 \
+  --preprocessing resize,to_gray,crop,crop_patches,standardization \
+  --downstream_preprocessing resize,crop,standardization \
+  --resize_size 292,292 \
+  --lr 0.1 \
+  --lr_scale_batch_size 128 \
+  --decay_epochs 60,90,120 \
+  --epochs 100 \
+  --warmup_epochs 5 \
+  --serving_input_shape None,64,64,3 \
+  --load_model /project/amr239/gma35/RotNet-Neural-Collapse/googleResultsRecreation/workdirs/LinearHead_Jig9_10Perms_40Epoch/checkpoint_last.pt \
+  --layer_extractor block2 \
+  --small_down_head true \
+  "$@"
