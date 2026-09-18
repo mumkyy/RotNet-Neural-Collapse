059ccbd76d7ea9229e33c14fea1887a71043a113
Assaf Izhar <assaf.izhar@gmail.com>
2026-09-17 20:01:13 -0400

    final minimal backbones + downstreams

---
+++ RNC/config/CIFAR10/RotNet/MSE/backbone/CIFAR10_RotNet_NIN4_MSE_NotCollapsed_NC1NC3Reg.py
@@ -0,0 +1,81 @@
+batch_size   = 128
+
+config = {}
+# set the parameters related to the training and testing set
+data_train_opt = {} 
+data_train_opt['batch_size'] = batch_size
+data_train_opt['unsupervised'] = True
+data_train_opt['epoch_size'] = None
+data_train_opt['random_sized_crop'] = False
+data_train_opt['dataset_name'] = 'cifar10'
+data_train_opt['split'] = 'train'
+
+data_test_opt = {}
+data_test_opt['batch_size'] = batch_size
+data_test_opt['unsupervised'] = True
+data_test_opt['epoch_size'] = None
+data_test_opt['random_sized_crop'] = False
+data_test_opt['dataset_name'] = 'cifar10'
+data_test_opt['split'] = 'test'
+
+config['data_train_opt'] = data_train_opt
+config['data_test_opt']  = data_test_opt
+config['max_num_epochs'] = 200
+
+net_opt = {}
+net_opt['num_classes'] = 4
+net_opt['num_stages']  = 4
+net_opt['use_avg_on_conv3'] = False
+
+networks = {}
+net_optim_params = {'optim_type': 'sgd', 'lr': 0.1, 'momentum':0.9, 'weight_decay': 0, 'nesterov': True, 'LUT_lr':[(60, 0.1),(120, 0.02),(160, 0.004),(200, 0.0008)]}
+networks['model'] = {'def_file': 'architectures/NetworkInNetwork.py', 'pretrained': None, 'opt': net_opt,  'optim_params': net_optim_params} 
+config['networks'] = networks
+
+# NC1 (nc_reg): reg log LW
+config['nc_reg'] = {
+    'layers': ['conv2', 'conv3', 'conv4', 'penult', 'classifier'],
+    'weights': {
+        'conv2': 0.005,
+        'conv3': 0.005,
+        'conv4': 0.005,
+        'penult': 0.005,
+        'classifier': 0.005,
+    },
+    'detach_sb': True,
+    'inverse': False,
+}
+
+pabs_layers = [
+    "conv2.Block2_ConvB3",
+    "conv3.Block3_ConvB3",
+    "conv4.Block4_ConvB3",
+    "classifier",
+]
+
+pabs_weight = 1e-3
+
+config["nc3_layerwise_pen"] = {
+    "layers": pabs_layers,
+
+    "weights": {
+        "conv2.Block2_ConvB3": pabs_weight,
+        "conv3.Block3_ConvB3": pabs_weight,
+        "conv4.Block4_ConvB3": pabs_weight,
+        "classifier": pabs_weight,
+    },
+    "no_svd": True,
+}
+
+
+# NC3 head (nc3_reg): reg
+config['nc3_reg'] = {
+    'last_layer': 'penult',
+    'classifier': 'classifier',
+    'lambdaNC3': 0.1,
+    'use_log': True,
+}
+criterions = {}
+criterions['loss'] = {'ctype':'MSELoss', 'opt':None}
+config['criterions'] = criterions
+config['algorithm_type'] = 'ClassificationModel'
